3e69237893598855842761d7bd5c73c2ab681f09
So-shi <king.eskey@gmail.com>
2019-04-17 23:37:41 +0900

    third-commit

--- js/game.js
+++ js/game.js
@@ -7,9 +7,9 @@ var ASSETS = {
         bg1: "img/bg1.jpg",
         hito1: "img/hito1.png",
         enemy1: "img/enemy1.png",
-        gu: "img/gu.jpg",
-        tyo: "img/tyo.jpg",
-        pa: "img/pa.jpg",
+        gu: "img/gu.png",
+        tyo: "img/tyo.png",
+        pa: "img/pa.png",
     },
 };
 
@@ -17,6 +17,7 @@ var MESSAGE_SPEED = 4; //1<n 低いほど早い
 var FONT_SIZE = 25;
 var SCREEN_WIDTH = 800;              // スクリーン幅
 var SCREEN_HEIGHT = 600;
+var isClosed = 1;   //テキストのフラグ
 
 // 最初のセリフ
 const MAINNTEXTS = [
@@ -31,7 +32,16 @@ const MAINNTEXTS = [
     "アソパソマソが現れた！！！",
     "【アソパソマソ】\nぼく、アソパソマソ！\n\n元気１００倍！アソパソマソ！！",
     "アソパソマソが勝負を仕掛けてきた！！！",
-    "少年はどの手を出す？？\n（上の画像を選択してね）",
+    "少年はどの手を出す？？\n（上の画像を選択してね）", //11
+    "君はぐーだね！",
+    "君はチョキだね！",
+    "君はパーだね！",
+    "相手はぐーだよ！", //15
+    "相手はチョキだよ！",
+    "相手はパーだよ！",
+    "勝ったね！\nやったね！",       //18
+    "負けたね！笑",
+    "あいこだよ！",
 ];
 //
 
@@ -57,7 +67,10 @@ phina.define('TitleScene', {
         label1.x = this.gridX.center();
         label1.y = this.gridY.center() / 3;
 
-        var label2 = Label('画面をクリック').addChildTo(this);
+        var label2 = Label({
+            text: '画面をクリック',
+            fill: 'white',
+        }).addChildTo(this);
         label2.x = this.gridX.center();
         label2.y = this.gridY.center();
     },
@@ -104,44 +117,105 @@ phina.define('MainScene', {
 
         this.setPhase();
 
-        //ラベルエリアがクリックされた場合
-        this.labelRect.onclick = function () {
-            if (this.textAll) {
-                this.nextText();
-            } else {
-                this.showAllText();
-            }
-        }
-
-        // this.labelRect.update = function (app) {
-        //     if (app.pointer.getPointingStart() || app.keyboard.getKeyDown("enter")) {
-        //         // if (this.labelRect.onpointstart) {
-        //         if (this.textAll) {//テキスト全部表示済み
+        // //ラベルエリアがクリックされた場合
+        // this.labelRect.onclick = function () {
+        //     if (isClosed == 0) {
+        //         if (this.textAll) {
         //             this.nextText();
-        //             // 次の背景に切替
-        //             this.setPhase();
         //         } else {
-        //             this.showAllText();   //テキスト全部表示してないなら、全表示
+        //             this.showAllText();
         //         }
-        //     } else {    //クリックされてないなら、文字送り
-        //         this.addChar();
         //     }
-
-        //     if (this.textAll) {
-        //         this.nextTriangle.show();
-        //     } else {
-        //         this.nextTriangle.hide();
-        //     }
+        // }
+        // this.labelRect.onpointstart = (e) => {
+        //     console.log(e.pointer.getPointingStart());
+        //     console.log("wa-");
         // }
     },
 
     //更新
+    // update: function (app) {
+    //     this.labelRect.addChar();
+    //     if (this.labelRect.textAll) {
+    //         this.setPhase();
+    //     }
+
+    //     if (this.labelRect.textAll) {
+    //         this.labelRect.nextTriangle.show();
+    //     } else {
+    //         this.labelRect.nextTriangle.hide();
+    //     }
+    // },
+    //更新
     update: function (app) {
-        //クリックかEnterキーの入力があった場合
-        this.labelRect.addChar();
-        if (this.labelRect.textAll) {
-            this.setPhase();
+        //クリックがあった場合
+        this.labelRect.onpointstart = (e) => {
+            if (isClosed == 1) { //タッチ有効か？基本ここで進める
+                if (this.labelRect.textAll) {//テキスト全部表示済み
+                    this.labelRect.nextText();
+                    // 次の背景に切替
+                    this.setPhase();
+                } else {
+                    this.labelRect.showAllText();
+                }
+            }
+            if (isClosed == 2) {    //ジャンケンの途中はこっち
+                if (this.labelRect.textAll) {
+                    switch (this.labelRect.textIndex) {
+                        case 12:
+                            this.labelRect.textIndex = this.index.x;
+                            this.labelRect.clearText();
+                            this.labelRect.charIndex = 0;
+                            this.labelRect.addChar();
+                            break;
+                        case 13:
+                            this.labelRect.textIndex = this.index.x;
+                            this.labelRect.clearText();
+                            this.labelRect.charIndex = 0;
+                            this.labelRect.addChar();
+                            break;
+                        case 14:
+                            this.labelRect.textIndex = this.index.x;
+                            this.labelRect.clearText();
+                            this.labelRect.charIndex = 0;
+                            this.labelRect.addChar();
+                            break;
+                    }
+
+                } else {
+                    this.labelRect.showAllText();
+                }
+                isClosed = 3;   //勝敗の途中
+            }
+            if (isClosed == 3) {    //勝敗の途中
+                if (this.labelRect.textAll) {
+                    this.labelRect.textIndex = this.index.y;
+                    this.labelRect.clearText();
+                    this.labelRect.charIndex = 0;
+                    this.labelRect.addChar();
+                    switch (this.labelRect.textIndex) {
+                        case 18://勝ち
+                            break;
+                        case 19://負け
+                            isClosed = 1;
+                            this.exit();
+                            break;
+                        case 20:
+                            this.labelRect.textIndex = 11;
+                            this.labelRect.clearText();
+                            this.labelRect.charIndex = 0;
+                            this.labelRect.addChar();
+                            isClosed = 0;
+                            this.setPhase();
+                            break;
+                    }
+                } else {
+                    this.labelRect.showAllText();
+                }
+            }
         }
+
+        this.labelRect.addChar();
 
         if (this.labelRect.textAll) {
             this.labelRect.nextTriangle.show();
@@ -155,7 +229,6 @@ phina.define('MainScene', {
     //     if (app.pointer.getPointingStart() || app.keyboard.getKeyDown("enter")) {
     //         // if (this.labelRect.onpointstart) {
     //         if (this.labelRect.textAll) {//テキスト全部表示済み
-    //             console.log("ok1");
     //             this.labelRect.nextText();
     //             // 次の背景に切替
     //             this.setPhase();
@@ -164,7 +237,6 @@ phina.define('MainScene', {
     //         }
     //     } else {
     //         this.labelRect.addChar();
-    //         console.log("ok2");
     //     }
 
     //     if (this.labelRect.textAll) {
@@ -175,7 +247,7 @@ phina.define('MainScene', {
     // },
 
     setPhase: function () {
-        console.log(this.labelRect.textIndex);
+        let self = this;    //labelRectに参照するためにselfに逃避
         switch (this.labelRect.textIndex) {
             case 4:
                 this.bg1 = this.setBackImg("bg1");
@@ -196,12 +268,57 @@ phina.define('MainScene', {
                 this.gu = this.setGu("gu");
                 this.tyo = this.setTyo("tyo");
                 this.pa = this.setPa("pa");
+
+                break;
+            case 11:
+                isClosed = 0; //画面タッチでのテキスト送りを無効に
                 //タッチイベントの登録
                 this.gu.setInteractive(true); this.tyo.setInteractive(true); this.pa.setInteractive(true);
-                this.gu.onclick = function () {
-                    //ここでthis.labelRect.textsの中身を変更したいができない
-                }
+                // this.gu.onclick = function () {
+                //     //ここでthis.labelRect.textsの中身を変更したいができない
 
+                // }
+                this.gu.onpointstart = (e) => {
+                    this.index = this.janken(1);
+                    //this.indexの中には敵の手、勝敗のインデックス
+                    this.setPhaseJan(12);
+                    this.gu.setInteractive(false); this.tyo.setInteractive(false); this.pa.setInteractive(false);
+                }
+                this.tyo.onpointstart = (e) => {
+                    this.index = this.janken(2);
+                    this.setPhaseJan(13);
+                    this.gu.setInteractive(false); this.tyo.setInteractive(false); this.pa.setInteractive(false);
+                }
+                this.pa.onpointstart = (e) => {
+                    this.index = this.janken(3);
+                    this.setPhaseJan(14);
+                    this.gu.setInteractive(false); this.tyo.setInteractive(false); this.pa.setInteractive(false);
+                }
+                break;
+        }
+    },
+    setPhaseJan: function (zibun) {
+        switch (zibun) {
+            case 12:
+                this.labelRect.textIndex = 12;
+                this.labelRect.clearText();
+                this.labelRect.charIndex = 0;
+                this.labelRect.addChar();
+                isClosed = 2;   //ジャンケン中のクリック有効化
+                break;
+            case 13:
+                this.labelRect.textIndex = 13;
+                this.labelRect.clearText();
+                this.labelRect.charIndex = 0;
+                this.labelRect.addChar();
+                isClosed = 2;   //ジャンケン中のクリック有効化
+                break;
+            case 14:
+                this.labelRect.textIndex = 14;
+                this.labelRect.clearText();
+                this.labelRect.charIndex = 0;
+                this.labelRect.addChar();
+                isClosed = 2;   //ジャンケン中のクリック有効化
                 break;
         }
     },
@@ -222,24 +339,63 @@ phina.define('MainScene', {
     setGu: function (imgName) {
         let img = Sprite(imgName).addChildTo(this.guPosi)
             .setPosition(this.gridX.center() - 30, this.gridY.center() / 4);
-        img.scaleX -= 0.96;
-        img.scaleY -= 0.96;
+        img.scaleX -= 0.4;
+        img.scaleY -= 0.4;
         return img;
     },
     setTyo: function (imgName) {
         let img = Sprite(imgName).addChildTo(this.guPosi)
             .setPosition(this.gridX.center() + 80, this.gridY.center() / 2 + 40);
-        img.scaleX -= 0.96;
-        img.scaleY -= 0.96;
+        img.scaleX -= 0.4;
+        img.scaleY -= 0.4;
         return img;
     },
     setPa: function (imgName) {
         let img = Sprite(imgName).addChildTo(this.guPosi)
             .setPosition(this.gridX.center() - 30, this.gridY.center());
-        img.scaleX -= 0.96;
-        img.scaleY -= 0.96;
+        img.scaleX -= 0.4;
+        img.scaleY -= 0.4;
         return img;
     },
+    janken: function (x) {      //敵の手がテキストで格納されているインデックス番号と勝敗のインデックス番号を返す。
+        let rand = Math.floor(Math.random() * 3 + 1);
+        switch (x) {
+            case 1:     //グーの場合
+                //let rand = Math.floor(Math.random() * 3 + 1);
+                switch (rand) {
+                    case 1:
+                        return { x: 15, y: 20 };
+                    case 2:
+                        return { x: 16, y: 18 };
+                    case 3:
+                        return { x: 17, y: 19 };
+                }
+                break;
+            case 2:     //チョキの場合
+                //let rand = Math.floor(Math.random() * 3 + 1);
+                switch (rand) {
+                    case 1:
+                        return { x: 15, y: 19 };
+                    case 2:
+                        return { x: 16, y: 20 };
+                    case 3:
+                        return { x: 17, y: 18 };
+                }
+                break;
+            case 3:     //パーの場合
+                //let rand = Math.floor(Math.random() * 3 + 1);
+                switch (rand) {
+                    case 1:
+                        return { x: 15, y: 18 };
+                    case 2:
+                        return { x: 16, y: 19 };
+                    case 3:
+                        return { x: 17, y: 20 };
+                }
+                break;
+        }
+
+    }
 
 
     // onclick: function () {
@@ -248,33 +404,32 @@ phina.define('MainScene', {
     // }
 });
 
+//============================================
+// ゲームオーバー
+//============================================
+phina.define('GameOverScene', {
+    superClass: 'CanvasScene',
 
-//============================================
-// マネージャーシーン
-//============================================
-phina.define('MyManagerScene', {
-    superClass: 'ManagerScene',
-    init: function () {
-        this.superInit({
-            width: SCREEN_WIDTH,
-            height: SCREEN_HEIGHT,
-            scenes: [
-                // タイトル
-                {
-                    label: 'マイタイトル',
-                    className: 'TitleScene',
-                    nextLabel: 'マイメイン'
-                },
-                // メイン
-                {
-                    label: "マイメイン",
-                    className: "MainScene",
-                    nextLabel: "マイタイトル"
-                }
-            ]
-        });
+    init: function (options) {
+        this.superInit(options);
+
+        // this.backgroundColor = '#444';
+        this.bg = Sprite("title").addChildTo(this);
+        this.bg.origin.set(0, 0);
+
+        var label1 = Label({
+            text: 'GAME OVER',
+            fill: 'white',
+            fontSize: 72,
+        }).addChildTo(this);
+        label1.x = this.gridX.center();
+        label1.y = this.gridY.center();
+    },
+    onclick: function () {
+        this.exit();
     }
 });
+
 
 // メイン処理
 phina.main(function () {
@@ -284,6 +439,24 @@ phina.main(function () {
         width: SCREEN_WIDTH,
         height: SCREEN_HEIGHT,
         assets: ASSETS,
+        scenes: [
+            {
+                className: 'TitleScene',
+                label: 'title',
+                nextLabel: 'main',
+            },
+
+            {
+                className: 'MainScene',
+                label: 'main',
+                nextLabel: 'gameover',
+            },
+            {
+                className: 'GameOverScene',
+                label: 'gameover',
+                nextLabel: 'title',
+            },
+        ],
     });
     document.body.appendChild(app.domElement);
     // app.replaceScene(MyManagerScene());
